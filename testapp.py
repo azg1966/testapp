333e48fce1040da3c8517899392df7d5eba78459
alex <alex@localhost>
2020-04-15 06:52:10 +0400

    init

---
+++ testapp.py
@@ -0,0 +1,14 @@
+from aiohttp import web
+
+routes = web.RouteTableDef()
+
+
+@routes.get('/')
+async def index(request):
+    return web.Response(text='Hello World')
+
+
+if __name__ == '__main__':
+    app = web.Application()
+    app.router.add_routes(routes)
+    web.run_app(app, host='0.0.0.0', port=8000)
